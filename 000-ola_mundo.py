d72b177b040f2fb881b0ebad3c7ae92d4d05126a
Darlon TI <darlonjosedj@gmail.com>
2020-03-09 21:03:18 -0400

    Exemplo de olá mundo - 2020.03.09 - 22:03

---
+++ 000-ola_mundo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+##
+# Dependencias
+##
+# pip3 install ipaddress
+
+##
+# Modulos
+##
+import ipaddress
+import socket
+import sys
+
+#print("Olá Mundo !!!! - DARLONTI")
+sys.stdout.write ("\nOlá Mundo - DarlonTI \n\n")
+
+sys.stderr.write ("\n\t Deu ruim !!!\n")
